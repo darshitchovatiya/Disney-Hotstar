f9b29dd2dece0ec4db7c83a3658b6a5ba1a4e6ec
darshit15-02 <darshitc18@gmail.com>
2024-12-23 21:31:38 +0530

    - create-login page UI complete and nav bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
 import './App.css';
+import Hender from './components/Hender';
 
 function App() {
   return (
     <div className="App">
-      <h2>Let build the Disney+ Hotstar</h2>
+      <Router>
+        <Hender/>
+          <Routes>
+            {/* <Route exact path="/" >
+              <Login />
+            </Route> */}
+            <Route  path="/" element={<Login />} />
+          </Routes>
+      </Router>
+      
     </div>
   );
 }
