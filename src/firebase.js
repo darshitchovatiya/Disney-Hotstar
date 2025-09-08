7c2967dee8469c17b9767cae5fdeff8579ab2a60
darshitchovatiya <darshitc18@gmail.com>
2025-09-08 18:32:35 +0530

    - firebase data add

--- src/firebase.js
+++ src/firebase.js
@@ -1,62 +1,29 @@
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-
-
-// const firebaseConfig = {
-//     apiKey: "AIzaSyBnry1cdZ2geUgR1w9oAhEE2gB20nveXtY",
-//     authDomain: "disneyplus-app-bb79c.firebaseapp.com",
-//     projectId: "disneyplus-app-bb79c",
-//     storageBucket: "disneyplus-app-bb79c.firebasestorage.app",
-//     messagingSenderId: "539495659114",
-//     appId: "1:539495659114:web:92b2b8fee75f4db40cfc13",
-//     measurementId: "G-0XGY4MWRKG"
-// };
-
-// const firebaseApp = initializeApp(firebaseConfig);
-// const db = firebaseApp.firestore();
-// const auth = firebaseApp.auth();
-// const provider = new firebaseApp.auth.GoogleAuthProvider();
-// const analytics = getAnalytics(firebaseApp);
-// const storage = firebaseApp.storage();
-
-// export {  auth, provider, analytics, storage };
-// export default db;
-
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"; // Import Firestore
 import { getAuth, GoogleAuthProvider } from "firebase/auth"; // Import Auth and Provider
 import { getAnalytics } from "firebase/analytics"; // Import Analytics
 import { getStorage } from "firebase/storage"; // Import Storage
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyBnry1cdZ2geUgR1w9oAhEE2gB20nveXtY",
-//     authDomain: "disneyplus-app-bb79c.firebaseapp.com",
-//     projectId: "disneyplus-app-bb79c",
-//     storageBucket: "disneyplus-app-bb79c.firebasestorage.app",
-//     messagingSenderId: "539495659114",
-//     appId: "1:539495659114:web:92b2b8fee75f4db40cfc13",
-//     measurementId: "G-0XGY4MWRKG"
-// };
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBnry1cdZ2geUgR1w9oAhEE2gB20nveXtY",
-  authDomain: "disneyplus-app-bb79c.firebaseapp.com",
-  projectId: "disneyplus-app-bb79c",
-  storageBucket: "disneyplus-app-bb79c.firebasestorage.app",
-  messagingSenderId: "539495659114",
-  appId: "1:539495659114:web:daf020bc4c5685c10cfc13",
-  measurementId: "G-M01HEN9ZPW"
+  apiKey: "AIzaSyDk8SnFmYN2vuUT_KxiHtaZvdSR5DE5EfA",
+  authDomain: "dineyplus-clone-da41d.firebaseapp.com",
+  projectId: "dineyplus-clone-da41d",
+  storageBucket: "dineyplus-clone-da41d.firebasestorage.app",
+  messagingSenderId: "730033084924",
+  appId: "1:730033084924:web:bfcb7aac502179eb31ba79",
+  measurementId: "G-HJPP0ST7V3"
 };
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 
 // Initialize services
-const db = getFirestore(firebaseApp); // Correct way to initialize Firestore
-const auth = getAuth(firebaseApp); // Correct way to initialize Auth
-const provider = new GoogleAuthProvider(); // Create a Google Auth provider
-const analytics = getAnalytics(firebaseApp); // Initialize Analytics
-const storage = getStorage(firebaseApp); // Initialize Storage
+const db = getFirestore(firebaseApp); 
+const auth = getAuth(firebaseApp);
+const provider = new GoogleAuthProvider(); 
+const analytics = getAnalytics(firebaseApp); 
+const storage = getStorage(firebaseApp); 
 
 export { auth, provider, analytics, storage };
 export default db;
